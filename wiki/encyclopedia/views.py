351e8546266583018b01c7ee78850d064ca64fa5
angelbear24 <angelbear24@ymail.com>
2022-05-02 09:27:06 -0500

    Added some test entries

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from . import utils
 
 # Create your views here.
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": utils.list_entries()
+    })
